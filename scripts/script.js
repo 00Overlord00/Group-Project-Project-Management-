40ed73604e578ec4562a8be2a1d916686d206e86
00Overlord00 <nick@Chelseas-MacBook-Air.local>
2016-06-09 11:30:36 -0500

    More minor fixes, functionality framework

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,7 @@
 console.log( 'Script: GO' );
+
+$(document).on( 'click', '#generateProject', function(){
+  $('body').append( '<div id = "projectArea"></div>' );
+  $('body').append( '<div id = "buttonArea"></div>' );
+  $( '#buttonArea' ).append( '<button id = "empGen"' );
+});
