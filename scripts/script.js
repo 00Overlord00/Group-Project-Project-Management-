f6eeef2cb40d7c265e35bafdf615f26c8dbcd5ee
00Overlord00 <nick@Chelseas-MacBook-Air.local>
2016-06-09 11:32:52 -0500

    Basic framework complete

--- scripts/script.js
+++ scripts/script.js
@@ -3,5 +3,5 @@ console.log( 'Script: GO' );
 $(document).on( 'click', '#generateProject', function(){
   $('body').append( '<div id = "projectArea"></div>' );
   $('body').append( '<div id = "buttonArea"></div>' );
-  $( '#buttonArea' ).append( '<button id = "empGen"' );
+  $( '#buttonArea' ).append( '<button id = "empGen">Generate Employee</div>' );
 });
