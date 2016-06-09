24c7cb1f1c9caff965cfdd9183b59de0b7535f82
00Overlord00 <nick@Chelseas-MacBook-Air.local>
2016-06-09 11:54:48 -0500

    minor fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,16 @@
 console.log( 'Script: GO' );
 
+var makeMyDay = function() {
+  $( 'projectArea' ).append( '' );
+};
+
+var wildCard = function(min, max) {
+  return Math.random() * (max - min) + min;
+};
+
 $(document).on( 'click', '#generateProject', function(){
   $('body').append( '<div id = "projectArea"></div>' );
   $('body').append( '<div id = "buttonArea"></div>' );
-  $( '#buttonArea' ).append( '<button id = "empGen">Generate Employee</div>' );
+  $( '#empGenDiv' ).append( '<button id = "empGen">Generate Employee</button>' );
+  console.log( parseInt(wildCard(1,9)) );
 });
