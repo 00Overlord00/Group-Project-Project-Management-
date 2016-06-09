a0660175af6b0cb6fab876b731046c9e22794292
00Overlord00 <nick@Chelseas-MacBook-Air.local>
2016-06-09 14:23:50 -0500

    MVP client

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,46 @@
 console.log( 'Script: GO' );
 
+$(document).ready( function() {
+
 var makeMyDay = function() {
-  $( 'projectArea' ).append( '' );
+  $( '#projectArea' ).append( '' );
 };
 
-var wildCard = function(min, max) {
+var wildCard = function(){
+  return parseInt(mysteryNumber(10, 60));
+};
+
+var mysteryNumber = function(min, max) {
   return Math.random() * (max - min) + min;
 };
 
 $(document).on( 'click', '#generateProject', function(){
-  $( '#projectArea' ).empty();
   $('body').append( '<div id = "projectArea"></div>' );
+  $( '#projectArea' ).empty();
+  $( '#empGenDiv' ).empty();
   $( '#empGenDiv' ).append( '<button id = "empGen">Generate Employee</button>' );
-  console.log( parseInt(wildCard(1,9)) );
+  // $( '#projectArea' ).append('<p>' + genCo() + '</p>');
+  var company = genCo();
+  $('#projectArea').append('<p>Project: ' + company.name + ' Logic: ' + company.logic + ' Front End: ' + company.frontEnd + ' Back End: ' + company.backEnd + '</p>');
+});
 
+var bigBusiness = function() {
+  var businessName;
+  var rando = wildCard();
+  if ( rando >25) {
+    businessName = "CrapCo.";
+  }else {
+    businessName = "HyperKids INC.";
+  } return businessName;
+};
+
+var genCo = function() {
+    var company = { name: 'undefined', logic: 'undefined', frontEnd: 'undefined', backEnd: 'undefined'};
+    company.name = bigBusiness();
+    company.logic = wildCard();
+    company.frontEnd = wildCard();
+    company.backEnd = wildCard();
+    console.log( company );
+    return company;
+  };
 });
